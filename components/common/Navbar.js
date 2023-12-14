f3c2a3ab0d0182eb483e027d88d9d353457c9516
Paolo <paodzon@gmail.com>
2023-12-14 22:42:29 +0800

    textcenter

--- components/common/Navbar.js
+++ components/common/Navbar.js
@@ -114,7 +114,7 @@ export default function Navbar() {
                                 {({ active }) => (
                                   <Link
                                     href={item.href || '#'}
-                                    className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                    className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700 text-center')}
                                   >
                                    {item.name}
                                   </Link>
